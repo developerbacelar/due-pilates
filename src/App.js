413e0b7419ec600f76ccfa0394e514cc9a08aaef
mbacelar <mbacelar@gazetadopovo.com.br>
2023-01-23 17:49:33 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         <h1>Studio de Pilates & Treinamento Funcional</h1>
         <span>Em breve um novo site para você!</span>
       </main>
-      <a className="btn-whatsapp-flutuante" href="https://api.whatsapp.com/send?phone=554199367407&text=Olá,%20vim%20pelo%20seu%20site!%20Gostaria%20de%20informações%20sobre%20uma%20futura%20aula%20experimental." target="_blank" rel="noopener">
+      <a className="btn-whatsapp-flutuante" href="https://api.whatsapp.com/send?phone=5541991256464&text=Olá,%20vim%20pelo%20seu%20site!%20Gostaria%20de%20informações%20sobre%20uma%20futura%20aula%20experimental." target="_blank" rel="noopener">
         <img src={logoWhatsApp}/>
       </a>
     </div>
