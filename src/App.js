26ced61b82dbe67fdaf3efd2ec924f40a22cbe56
mbacelar <mbacelar@gazetadopovo.com.br>
2023-01-23 16:23:54 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import './Reset.css';
+import logo from './due-logo.svg';
+import logoWhatsApp from './logo-whatsApp.svg';
 
 function App() {
   return (
     <div className="App">
-      <h1>Teste</h1>
+      <main>
+        <img src={logo} alt="Logo Due Studio Personal"/>
+        <h1>Studio de Pilates & Treinamento Funcional</h1>
+        <span>Em breve um novo site para você!</span>
+      </main>
+      <a className="btn-whatsapp-flutuante" href="https://api.whatsapp.com/send?phone=554199367407&text=Olá,%20vim%20pelo%20seu%20site!%20Gostaria%20de%20informações%20sobre%20uma%20futura%20aula%20experimental." target="_blank" rel="noopener">
+        <img src={logoWhatsApp}/>
+      </a>
     </div>
   );
 }
